96f3254dc02cc3be7431763698a34c044f1468bd
MattMori <mateus5310@live.com>
2024-01-24 23:53:02 -0300

    Adição de Hovers nas paginas de bandas, playlists  e noticias.

--- src/pages/noticias/Noticias.jsx
+++ src/pages/noticias/Noticias.jsx
@@ -5,18 +5,18 @@ const Noticias = () => {
     <div className="Noticias">
       <h1> Noticias</h1>
       <div className="Noticias-quadrados">
-        <a href="/Noticia"><div className="quadrado"> Noticia 1</div></a>
-        <div className="quadrado">Noticia 2</div>
-        <div className="quadrado">Noticia 3</div>
-        <div className="quadrado">Noticia 4</div>
-        <div className="quadrado">Noticia 5</div>
-        <div className="quadrado">Noticia 6</div>
-        <div className="quadrado">Noticia 7</div>
-        <div className="quadrado">Noticia 8</div>
-        <div className="quadrado">Noticia 9</div>
-        <div className="quadrado">Noticia 10</div>
-        <div className="quadrado">Noticia 11</div>
-        <div className="quadrado">Noticia 12</div>
+        <a href="/Noticia"><div className="Noticia-quadrado"> Noticia 1</div></a>
+        <div className="Noticia-quadrado">Noticia 2</div>
+        <div className="Noticia-quadrado">Noticia 3</div>
+        <div className="Noticia-quadrado">Noticia 4</div>
+        <div className="Noticia-quadrado">Noticia 5</div>
+        <div className="Noticia-quadrado">Noticia 6</div>
+        <div className="Noticia-quadrado">Noticia 7</div>
+        <div className="Noticia-quadrado">Noticia 8</div>
+        <div className="Noticia-quadrado">Noticia 9</div>
+        <div className="Noticia-quadrado">Noticia 10</div>
+        <div className="Noticia-quadrado">Noticia 11</div>
+        <div className="Noticia-quadrado">Noticia 12</div>
       </div>
     </div>
   );
